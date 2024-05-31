8cf2ffeb82350c54e29dd91664451aa2a370ec84
Jatin Vashisth <136696926+jatinvashistha@users.noreply.github.com>
2024-05-31 09:21:33 +0530

    Update Application.js

--- src/redux/actions/Application/Application.js
+++ src/redux/actions/Application/Application.js
@@ -1,66 +1,75 @@
+ import axios from "axios";
 import { MAIN_URL } from "../../../URLS/config";
-import { EmployeeApplicationFailure, employeeApplicationRequest, employeeApplicationSuccess, jobSeekerGetAllApplicationFailure, jobSeekerGetAllApplicationRequest, jobSeekerGetAllApplicationSuccess } from "../../reducers/Application/Application";
-import axios from 'axios'
-export const postApplication = async (options )=> {
+import {
+  EmployeeApplicationFailure,
+  employeeApplicationRequest,
+  employeeApplicationSuccess,
+  jobSeekerGetAllApplicationFailure,
+  jobSeekerGetAllApplicationRequest,
+  jobSeekerGetAllApplicationSuccess,
+} from "../../reducers/Application/Application";
+
+const instance = axios.create({
+  baseURL: MAIN_URL,
+  withCredentials: true,
+});
+const token = localStorage.getItem("token");
+
+export const postApplication = async (options) => {
   try {
-    console.log('the application is', options)
-    const data = await axios.post(`${MAIN_URL}/application/postapplication`, 
-      options
-    ,{
-      withCredentials : true
+    const {name,email,phone, coverLetter,address,city ,country,jobId,resume} = options
+    console.log("the application is", options);
+    const data = await instance.post("/application/postapplication", {
+      name,
+      email,
+      phone,
+      coverLetter,
+      address,
+      city,
+      country,
+      jobId,
+      resume,
+      token
     });
-      
-    return data.data
-    
-    } catch (e) {
-       console.log("the error is ", e);
-    }
-}
+    return data.data;
+  } catch (e) {
+    console.log("the error is ", e);
+  }
+};
+
 export const employeeGetAllApplication = (options) => async (dispatch) => {
   try {
     dispatch(employeeApplicationRequest());
-    const res = await axios.get(
-      `${MAIN_URL}/application/employeeapplications`,
-      {
-        withCredentials: true,
-      }
-    );
-    
-
+    const res = await instance.put("/application/employeeapplications", {
+      token
+    });
     dispatch(employeeApplicationSuccess(res.data.applications));
   } catch (e) {
     dispatch(EmployeeApplicationFailure(e.message));
     console.log("the error is ", e);
   }
 };
+
 export const jobSeekerGetAllApplication = (options) => async (dispatch) => {
   try {
     dispatch(jobSeekerGetAllApplicationRequest());
-    const res = await axios.get(
-      `${MAIN_URL}/application/jobseekerapplications`, {
-        withCredentials : true
-      }
-    );
+    const res = await instance.put("/application/jobseekerapplications", {
+      token
+    });
     dispatch(jobSeekerGetAllApplicationSuccess(res.data.applications));
   } catch (e) {
     dispatch(jobSeekerGetAllApplicationFailure(e.message));
     console.log("the error is ", e);
   }
 };
+
 export const jobSeekerDeleteApplication = async (id) => {
   try {
-
-    const data = await axios.delete(
-      `${MAIN_URL}/application/deleteapplication/${id}`, {
-        withCredentials : true
-      }
-    );
-
-return data.data
-
-
-
+    const data = await instance.put(`/application/deleteapplication/${id}`, {
+      token
+    });
+    return data.data;
   } catch (e) {
-    console.log('the error is ',e);
+    console.log("the error is ", e);
   }
 };
